81b5cd9cb2eea0793e2956fcbaff951e9fc8db56
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-24 20:19:50 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 
 const App = () => {
 
-  const StartTrivia = async () => {
+  const startTrivia = async () => {
 
   }
 
@@ -16,7 +17,19 @@ const App = () => {
   }
 
 
-  return (<div className="App">Quiz</div>
+  return (
+    <div className="App">
+      <h1>Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
   );
 }
 
