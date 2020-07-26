117faaa08e1bb4209fba054800953a93c0c9f826
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-26 20:09:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import QuestionCard from './components/QuestionCard';
 
 const App = () => {
 
+  const TOTAL_QUESTIONS = 10;
+
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [number, setNumber] = useState(0);
@@ -32,7 +34,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
